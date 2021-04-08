1e9dacada7a0d9fed2d293cc4e90cf96c8a269d9
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-04-08 15:14:11 -0500

    typeScript configurations and variables

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+// Variales in typeScript:
+var a = "1";
+console.log("aaa", a);
+var hello = "world";
+//  specify types explicily:
+var hi = "hi";
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+// Variales in typeScript:
+const a ="1";
+console.log("aaa", a)
+
+let hello = "world";
+
+//  specify types explicily:
+let hi:string = "hi"
